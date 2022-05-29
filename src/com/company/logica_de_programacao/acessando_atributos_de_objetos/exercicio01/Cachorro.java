7b74f5f9e285d5ac5e9457433ef45f4c4a8a9c72
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-29 10:11:30 -0300

    Delete src/com/company/logica_de_programacao/acessando_atributos_de_objetos directory

--- src/com/company/logica_de_programacao/acessando_atributos_de_objetos/exercicio01/Cachorro.java
+++
@@ -1,9 +0,0 @@
-package com.company.logica_de_programacao.acessando_atributos_de_objetos.exercicio01;
-
-public class Cachorro {
-
-    String nome;
-    String raca;
-    char sexo;
-    int idade;
-}
